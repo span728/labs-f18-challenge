f96d792cff461b8335c5caa230a8033f1ff74b82
Stephanie Pan <sp3507@columbia.edu>
2018-09-07 18:46:44 -0400

    finished Task 3 in app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+import requests
+from string import Template
 from flask import Flask, render_template
+import json
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template('index.html')
+	return render_template('index.html')
+
+#@app.route('/<pokename>', methods=['GET'])
+#def pokename_page(pokename):
+ #   return(NAME_TEMPLATE.substitute(name=pokename, idnum='4'))
+
+@app.route("/pokemon/<query>/") 
+def getPokemon(query): 
+	r = requests.get('http://pokeapi.co/api/v2/pokemon/' + query + '/', verify=False)
+	pokemon = json.loads(r.text)
+	if query.isdigit() : 
+		return 'The pokemon with id ' + query + ' is ' + str(pokemon['name'])
+	else:
+		return query + ' has id ' + str(pokemon['id'])
 
 
 if __name__ == '__main__':
-    app.run()
+   app.run(use_reloader=True, debug=True)
